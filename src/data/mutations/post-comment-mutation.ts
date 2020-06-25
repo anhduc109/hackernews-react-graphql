6863b808942a330d543c05e8d4827d2ab0b0de4b
anhduc109 <ducpham1098@gmail.com>
2020-06-26 00:01:55 +0300

    remove submitterId

--- src/data/mutations/post-comment-mutation.ts
+++ src/data/mutations/post-comment-mutation.ts
@@ -2,7 +2,7 @@ import gql from 'graphql-tag';
 
 export const POST_COMMENT_MUTATION = gql`
   mutation PostComment($id: Int!, $parent: Int!, $text: String!) {
-    postComment(id: $id, parent: $parent, submitterId: $submitterId, text: $text) {
+    postComment(id: $id, parent: $parent, text: $text) {
       parent
       text
     }
