d87f58f1c5d781fa2d5112ce9f39cb9dbc2efb97
anhduc109 <ducpham1098@gmail.com>
2020-06-26 01:32:56 +0300

    add git library and isGif

--- src/components/comment-box.tsx
+++ src/components/comment-box.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { useMutation } from 'react-apollo';
 import Router from 'next/router';
+import Picker from 'react-giphy-picker';
 
 import { NewsItemModel } from '../data/models';
 import { POST_COMMENT_MUTATION } from '../data/mutations/post-comment-mutation';
@@ -37,10 +38,17 @@ export function CommentBox(props: ICommentsBoxProps): JSX.Element {
           <input type="hidden" name="parent" value="15237896" />
           <input type="hidden" name="goto" value="item?id=15237896" />
           <input type="hidden" name="hmac" value="02641d0660c89c1a83ccf0d171e42497d10d2135" />
-          <textarea name="text" onChange={(e) => setText(e.target.value)} rows={6} cols={60} />
+          <textarea
+            value={text}
+            name="text"
+            onChange={(e) => setText(e.target.value)}
+            rows={6}
+            cols={60}
+          />
           <br />
           <br />
           <input type="submit" value="add comment" />
+          <Picker onSelected={(item) => setText(item)} />
         </form>
       </td>
     </tr>
